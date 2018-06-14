f82f9e51c2a4abc8d3c8f8dd0b32ae56a9b44969
Trisha Taylor <jttacj2@gmail.com>
2018-06-14 17:43:58 -0600

    Added Table

---
+++ assignments/lesson-8/scripts/currentdate.js
@@ -0,0 +1,18 @@
+/*get current date*/
+
+var d = new Date();
+var days = ["Sunday", 
+			"Monday", 
+			"Tuesday", 
+			"Wednesday", 
+			"Thursday", 
+			"Friday", 
+			"Saturday"];
+var months = ["January", 
+			  "February", 
+			  "March", 
+			  "April", 
+			  "May", 
+			  "June", 
+			  "July", "August", "September", "October", "November", "December"];
+document.getElementById("js").innerHTML = days[d.getDay()]+' '+ d.getDate() +' '+ months[d.getMonth()] +' '+ d.getFullYear();
