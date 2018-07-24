aa08a06a0cb517a6293a763440a37c55dd61b829
Trisha Taylor <jttacj2@gmail.com>
2018-07-23 20:41:34 -0600

    Update

--- assignments/the-mountain-spoke/scripts 4.07.32 PM/accordion.js
+++ assignments/the-mountain-spoke/scripts 4.07.32 PM/accordion.js
@@ -1,9 +1,13 @@
-$(".accordion > li").click(function(){
-	if(false === $(this).next().is(':visible')) {
-		$('.accordion > ul').slideUp(300);
-	}
-	$(this).next().slideToggle(300);
-})
 
-$('.accordion > ul:eq(0)').show();
 
+$('.accordion > li').hover(function(){
+			//This code executes when the mouse pointer is overe the list item
+
+			$(this).addClass('openSesame');
+
+		}, function(){
+			//This code executes when the mouse pointer leaves the list item
+
+			$(this).removeClass('openSesame');
+
+		});
